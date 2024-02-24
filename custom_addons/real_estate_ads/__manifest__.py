1a42d371c123d1e4203dff90dc5c025f9e5b13b2
halyna <1208petrova@gmail.com>
2024-02-24 14:25:48 +0200

    create project structure and gitignore file

---
+++ custom_addons/real_estate_ads/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Real Estate Ads",
+    "version": "1.0",
+    "author": "Halyna",
+    "description": """
+        Real Estate model to show available properties
+    """,
+    "category": "Sales",
+    "depends": ["base"],
+    "data": [
+
+    ],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3"
+}
