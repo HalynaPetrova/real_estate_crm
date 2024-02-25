616446fbfeb20729791f14744c4f874bfc96b669
halyna <1208petrova@gmail.com>
2024-02-25 15:12:23 +0200

    add a decorators to remove rejected offers daily and check if the ads deadline and creation date match

--- custom_addons/real_estate_ads/models/property_offer.py
+++ custom_addons/real_estate_ads/models/property_offer.py
@@ -1,6 +1,8 @@
 from odoo import fields, models, api
 from datetime import timedelta
 
+from odoo.odoo.exceptions import ValidationError
+
 
 class PropertyOffer(models.Model):
     _name = 'estate.property.offer'
@@ -15,6 +17,10 @@ class PropertyOffer(models.Model):
     validity = fields.Integer(string="Validity", default=7)
     deadline = fields.Date(string="Deadline", compute="_compute_deadline", inverse="_inverse_deadline")
     creation_date = fields.Date(string="Creation Date", default="_set_create_date")
+
+    @api.autovacuum
+    def _clean_offers(self):
+        self.search([('statys', '=', 'refused')]).unlick()
 
     @api.model
     def _set_create_date(self):
@@ -34,3 +40,14 @@ class PropertyOffer(models.Model):
                 rec.validity = (rec.deadline - rec.creation_date).days
             else:
                 rec.validity = False
+
+    # _sql_constraints = [
+    #     ('check_validity', 'check(validity > 0)', "Date must be greater than or equal to creation")
+    # ]
+
+    @api.constrains("validity")
+    def _check_validity(self):
+        for rec in self:
+            if rec.deadline <= rec.creation_date:
+                raise ValidationError("Date must be greater than or equal to creation")
+
