fee46bc88f7ce6ae1b5ebc2752b496ad8e5be8a0
halyna <1208petrova@gmail.com>
2024-02-26 18:12:46 +0200

    fix code with black

--- custom_addons/real_estate_ads/models/property.py
+++ custom_addons/real_estate_ads/models/property.py
@@ -2,17 +2,22 @@ from odoo import fields, models, api, _
 
 
 class Property(models.Model):
-    _name = 'estate.property'
-    _description = 'Estate Properties'
+    _name = "estate.property"
+    _description = "Estate Properties"
 
     name = fields.Char(string="Name", required=True)
-    state = fields.Selection([
-        ('new', 'New'),
-        ('received', 'Offer Received'),
-        ('accepted', 'Offer Accepted'),
-        ('sold', 'Sold'),
-        ('cancel', 'Cancel')
-    ], default='new', string="Status", group_expand="_expand_state")
+    state = fields.Selection(
+        [
+            ("new", "New"),
+            ("received", "Offer Received"),
+            ("accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancel", "Cancel"),
+        ],
+        default="new",
+        string="Status",
+        group_expand="_expand_state",
+    )
     description = fields.Text(string="Description")
     postcode = fields.Char(string="Postcode")
     date_availability = fields.Date(string="Available Form")
@@ -26,24 +31,31 @@ class Property(models.Model):
     garden = fields.Boolean(string="Garden", default=False)
     garden_area = fields.Integer(string="Garden Area(sqm)")
     garden_orientation = fields.Selection(
-        [('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
-        string="Garden Orientation", default='north')
-    type_id = fields.Many2one('estate.property.type', string="Property Type")
-    tag_ids = fields.Many2many('estate.property.tag', string="Property Tag")
-    offer_ids = fields.One2many('estate.property.offer', 'property_id', string="Offers")
-    sales_id = fields.Many2one('res.users', string="Salesman")
-    buyer_id = fields.Many2one('res.partner', string="Buyer", domain=[('is_company', '=', True)])
+        [("north", "North"), ("south", "South"), ("east", "East"), ("west", "West")],
+        string="Garden Orientation",
+        default="north",
+    )
+    type_id = fields.Many2one("estate.property.type", string="Property Type")
+    tag_ids = fields.Many2many("estate.property.tag", string="Property Tag")
+    offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
+    sales_id = fields.Many2one("res.users", string="Salesman")
+    buyer_id = fields.Many2one(
+        "res.partner", string="Buyer", domain=[("is_company", "=", True)]
+    )
     total_area = fields.Integer(string="Total Area")
-    phone = fields.Char(string='Phone', related='buyer_id.phone')
+    phone = fields.Char(string="Phone", related="buyer_id.phone")
     offer_count = fields.Integer(srting="Offer Count", compute="_compute_offer_count")
-    currency_id = fields.Many2one("res.currency", string="Currency",
-                                  default=lambda self: self.env.user.company_id.currency_id)
+    currency_id = fields.Many2one(
+        "res.currency",
+        string="Currency",
+        default=lambda self: self.env.user.company_id.currency_id,
+    )
 
     @api.onchange("living_area", "garden_area")
     def _onchange_total_area(self):
         self.total_area = self.living_area + self.garden_area
 
-    @api.depends('offer_ids')
+    @api.depends("offer_ids")
     def _compute_offer_count(self):
         for rec in self:
             rec.offer_count = len(rec.offer_ids)
@@ -58,16 +70,16 @@ class Property(models.Model):
         }
 
     def action_sold(self):
-        self.state = 'sold'
+        self.state = "sold"
 
     def action_cancel(self):
-        self.state = 'cancel'
+        self.state = "cancel"
 
-    @api.depends('offer_ids')
+    @api.depends("offer_ids")
     def _compute_best_price(self):
         for rec in self:
             if rec.offer_ids:
-                rec.best_offer = max(rec.offer_ids.mapped('price'))
+                rec.best_offer = max(rec.offer_ids.mapped("price"))
             else:
                 rec.best_offer = 0
 
@@ -76,21 +88,19 @@ class Property(models.Model):
         return "Estate Property - %s" % self.name
 
     def _expand_state(self, states, domain, order):
-        return [
-            key for key, dummy in type(self).state.selection
-        ]
+        return [key for key, dummy in type(self).state.selection]
 
 
 class PropertyType(models.Model):
-    _name = 'estate.property.type'
-    _description = 'Property Type '
+    _name = "estate.property.type"
+    _description = "Property Type "
 
     name = fields.Char(string="Name", required=True)
 
 
 class PropertyTag(models.Model):
-    _name = 'estate.property.tag'
-    _description = 'Property Tag '
+    _name = "estate.property.tag"
+    _description = "Property Tag "
 
     name = fields.Char(string="Name", required=True)
     color = fields.Integer(string="Color")
--- custom_addons/real_estate_ads/models/property_offer.py
+++ custom_addons/real_estate_ads/models/property_offer.py
@@ -5,23 +5,28 @@ from odoo.exceptions import ValidationError
 
 
 class PropertyOffer(models.Model):
-    _name = 'estate.property.offer'
-    _description = 'Estate Property Offers'
+    _name = "estate.property.offer"
+    _description = "Estate Property Offers"
 
-    name = fields.Char(string="Description", compute='_compute_name')
-    price = fields.Monetary(string='Price')
+    name = fields.Char(string="Description", compute="_compute_name")
+    price = fields.Monetary(string="Price")
     status = fields.Selection(
-        [('accepted', 'Accepted'), ('refused', 'Refused')],
-        string="Status")
-    partner_id = fields.Many2one('res.partner', string="Customer")
-    property_id = fields.Many2one('estate.property', string="Property")
+        [("accepted", "Accepted"), ("refused", "Refused")], string="Status"
+    )
+    partner_id = fields.Many2one("res.partner", string="Customer")
+    property_id = fields.Many2one("estate.property", string="Property")
     validity = fields.Integer(string="Validity", default=7)
-    deadline = fields.Date(string="Deadline", compute="_compute_deadline", inverse="_inverse_deadline")
+    deadline = fields.Date(
+        string="Deadline", compute="_compute_deadline", inverse="_inverse_deadline"
+    )
     creation_date = fields.Date(string="Creation Date", default=fields.Date.today)
-    currency_id = fields.Many2one("res.currency", string="Currency",
-                                  default=lambda self: self.env.user.company_id.currency_id)
+    currency_id = fields.Many2one(
+        "res.currency",
+        string="Currency",
+        default=lambda self: self.env.user.company_id.currency_id,
+    )
 
-    @api.depends('property_id', 'partner_id')
+    @api.depends("property_id", "partner_id")
     def _compute_name(self):
         for rec in self:
             if rec.partner_id and rec.property_id:
@@ -33,7 +38,7 @@ class PropertyOffer(models.Model):
     def _set_create_date(self):
         return fields.Date.today()
 
-    @api.depends('validity', 'creation_date')
+    @api.depends("validity", "creation_date")
     def _compute_deadline(self):
         for rec in self:
             if rec.creation_date and rec.validity:
@@ -43,7 +48,7 @@ class PropertyOffer(models.Model):
 
     @api.autovacuum
     def _clean_offers(self):
-        self.search([('statys', '=', 'refused')]).unlick()
+        self.search([("statys", "=", "refused")]).unlick()
 
     # _sql_constraints = [
     #     ('check_validity', 'check(validity > 0)', "Date must be greater than or equal to creation")
@@ -63,38 +68,44 @@ class PropertyOffer(models.Model):
                 rec.validity = False
 
     def _validate_accepted_offer(self):
-        offer_ids = self.env['estate.property.offer'].search([
-            ("property_id", "=", self.property_id.id),
-            ("status", "=", "accepted"),
-        ])
+        offer_ids = self.env["estate.property.offer"].search(
+            [
+                ("property_id", "=", self.property_id.id),
+                ("status", "=", "accepted"),
+            ]
+        )
         if offer_ids:
             raise ValidationError("You have already accepted")
 
     def action_accept_offer(self):
         if self.property_id:
             self._validate_accepted_offer()
-            self.property_id.write({
-                "selling_price": self.price,
-                "state": "accepted",
-            })
+            self.property_id.write(
+                {
+                    "selling_price": self.price,
+                    "state": "accepted",
+                }
+            )
         self.status = "accepted"
 
     def action_decline_offer(self):
         self.status = "refused"
         if all(self.property_id.offer_ids.mapped("status")):
-            self.property_id.write({
-                "selling_price": 0,
-                "state": "received",
-            })
+            self.property_id.write(
+                {
+                    "selling_price": 0,
+                    "state": "received",
+                }
+            )
 
     def extend_offer_deadline(self):
-        active_ids = self._context.get('active_ids', [])
+        active_ids = self._context.get("active_ids", [])
         if active_ids:
-            offer_ids = self.env['estate.property.offer'].browse(active_ids)
+            offer_ids = self.env["estate.property.offer"].browse(active_ids)
             for offer in offer_ids:
                 offer.validity = 10
 
     def _extend_offer_deadline(self):
-        offer_ids = self.env['estate.property.offer'].search([])
+        offer_ids = self.env["estate.property.offer"].search([])
         for offer in offer_ids:
             offer.validity = offer.validity + 1
